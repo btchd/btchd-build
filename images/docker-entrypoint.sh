234e1da7c5c14b728e7dcbd9064f7f9654b85e9d
BitcoinHD Master <master@btchd.org>
2020-07-01 16:52:37 +0800

    use "set -e" to interrupt scripts on error

--- images/docker-entrypoint.sh
+++ images/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
+
 set -e
 
 # build dest dir
 BUILD_DIR=/workspace_build/$BUILD_HOST
-mkdir -p $BUILD_DIR && cd $BUILD_DIR || exit 1
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 # for custom depends build
 DEPENDS_DIR=/workspace_build/depends
@@ -14,24 +15,24 @@ make -C /workspace/depends -j$BUILD_THREADS \
     WORK_PATH=$DEPENDS_DIR/work \
     BASE_CACHE=$DEPENDS_DIR/built \
     SDK_PATH=$DEPENDS_DIR/SDKs \
-    FALLBACK_DOWNLOAD_PATH=https://download.bitcoinabc.org/depends-sources || exit 1
+    FALLBACK_DOWNLOAD_PATH=https://download.bitcoinabc.org/depends-sources
 
 # build
-/workspace/autogen.sh && /workspace/configure $BUILD_ARGS || exit 1
-make clean && make -j$BUILD_THREADS || exit 1
+/workspace/autogen.sh && /workspace/configure $BUILD_ARGS
+make clean && make -j$BUILD_THREADS
 
 # deploy
 if [ "$(echo $BUILD_HOST | grep mingw32)" != "" ]; then
     # for windows
     ## fixes nsi
-    sed -i "s/\/workspace\/release/\/workspace_build\/$BUILD_HOST\/release/g" $BUILD_DIR/share/setup.nsi || exit 1
-    sed -i "s/\/workspace\/BitcoinHD/\/workspace_build\/$BUILD_HOST\/BitcoinHD/g" $BUILD_DIR/share/setup.nsi || exit 1
-    make -j$BUILD_THREADS DESTDIR=$BUILD_DIR/release install || exit 1
+    sed -i "s/\/workspace\/release/\/workspace_build\/$BUILD_HOST\/release/g" $BUILD_DIR/share/setup.nsi
+    sed -i "s/\/workspace\/BitcoinHD/\/workspace_build\/$BUILD_HOST\/BitcoinHD/g" $BUILD_DIR/share/setup.nsi
+    make -j$BUILD_THREADS DESTDIR=$BUILD_DIR/release install
 fi
 if [ "$(echo $BUILD_HOST | grep linux)" != "" ]; then
     # for linux like
-    make -j$BUILD_THREADS DESTDIR=$BUILD_DIR/release install || exit 1
+    make -j$BUILD_THREADS DESTDIR=$BUILD_DIR/release install
     $BUILD_HOST-strip $BUILD_DIR/release/bin/btchd*
 else
-    make -j$BUILD_THREADS deploy || exit 1
+    make -j$BUILD_THREADS deploy
 fi
